ee06c76dbc763e4bf616e34d890aabae73984288
Dipanjan Pradip Karmakar <dipanjankarmakar43@gmail.com>
2025-07-31 22:23:41 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const io = new Server(server, {
   cors: { origin: "*", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://linkedin-ebmh.onrender.com",
+    credentials: true, // if using cookies/auth
+  })
+);
 app.use(express.json());
 
 app.use((req, res, next) => {
